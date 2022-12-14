332d6963141f0ad7a0c4d0e1e40fc6864b61a1eb
Prem Tiwari <prem.tiwari@finastra.com>
2022-12-14 10:02:47 +0530

    Reverse Array

---
+++ ReverseAnArray/ReverseArray.java
@@ -0,0 +1,28 @@
+package ReverseAnArray;
+
+import java.util.Arrays;
+
+class ReverseArray {
+
+    static int[] reverseArray(int [] arr) {
+        int i = 0;
+        int j = arr.length-1;
+        
+        while(i != j){
+            if(j < i)
+                break;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5,6,7,8,9};
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(reverseArray(arr)));
+    }
+}
