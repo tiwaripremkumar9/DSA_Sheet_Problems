ed5efa251b172107ee0700814aa17a5aca43bc91
Prem Tiwari <prem.tiwari@finastra.com>
2022-12-18 00:06:05 +0530

    Sort an Array containing of elements 0, 1 and 2

---
+++ SortArraysOf01and2/Solution.java
@@ -0,0 +1,42 @@
+package SortArraysOf01and2;
+
+import java.util.Arrays;
+
+/*
+ * Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo
+ * 
+ */
+
+public class Solution {
+
+    // Brute Force Way
+    public static int [] sortTheArray(int [] arr){
+        int sum = 0;
+        int numberOfTwos = 0;
+        int l = arr.length;
+        for(int i = 0; i < l; i++){
+            if(arr[i] == 2)
+                numberOfTwos++; // find number of 2s in the array (alternatively any other than 2 can also be chosen)
+            sum = sum+arr[i]; //find the total sum of elements of the array
+        }
+        int ones = sum - (2*numberOfTwos); // find number of ones
+        int zeroes = l - (ones + numberOfTwos); // find number of zeroes
+        for(int i = 0; i < zeroes; i++){
+            arr[i] = 0;
+        }
+        for(int i = zeroes; i < zeroes+ones; i++){
+            arr[i] = 1;
+        }
+        for(int i = zeroes+ones; i < l; i++){
+            arr[i] = 2;
+        }
+        return arr;
+    }
+
+
+    
+    public static void main(String[] args) {
+        int [] arr = {1,2,0,0,2,0,2,1,0,0,0,0,2,2,1,1,1};
+        System.out.println(Arrays.toString(sortTheArray(arr)));
+    }
+}
