9aabf6746f2cb6a6a633884ea37aeb89d4d50cff
Prem Tiwari <prem.tiwari@finastra.com>
2022-12-19 11:14:56 +0530

    Other better approach

--- SortArraysOf01and2/Solution.java
+++ SortArraysOf01and2/Solution.java
@@ -10,24 +10,52 @@ import java.util.Arrays;
 public class Solution {
 
     // Brute Force Way
+    // public static int [] sortTheArray(int [] arr){
+    //     int sum = 0;
+    //     int numberOfTwos = 0;
+    //     int l = arr.length;
+    //     for(int i = 0; i < l; i++){
+    //         if(arr[i] == 2)
+    //             numberOfTwos++; // find number of 2s in the array (alternatively any other than 2 can also be chosen)
+    //         sum = sum+arr[i]; //find the total sum of elements of the array
+    //     }
+    //     int ones = sum - (2*numberOfTwos); // find number of ones
+    //     int zeroes = l - (ones + numberOfTwos); // find number of zeroes
+    //     for(int i = 0; i < zeroes; i++){
+    //         arr[i] = 0;
+    //     }
+    //     for(int i = zeroes; i < zeroes+ones; i++){
+    //         arr[i] = 1;
+    //     }
+    //     for(int i = zeroes+ones; i < l; i++){
+    //         arr[i] = 2;
+    //     }
+    //     return arr;
+    // }
+
+
+    //other better approach
     public static int [] sortTheArray(int [] arr){
-        int sum = 0;
-        int numberOfTwos = 0;
         int l = arr.length;
+        int numberOfTwos = 0;
+        int numberOfTOnes = 0;
+        int numberOfZeroes = 0;
         for(int i = 0; i < l; i++){
             if(arr[i] == 2)
-                numberOfTwos++; // find number of 2s in the array (alternatively any other than 2 can also be chosen)
-            sum = sum+arr[i]; //find the total sum of elements of the array
+                numberOfTwos++;
+            if(arr[i] == 1)
+                numberOfTOnes++;
+            if(arr[i] == 0)
+                numberOfZeroes++;
+            
         }
-        int ones = sum - (2*numberOfTwos); // find number of ones
-        int zeroes = l - (ones + numberOfTwos); // find number of zeroes
-        for(int i = 0; i < zeroes; i++){
+        for(int i = 0; i < numberOfZeroes; i++){
             arr[i] = 0;
         }
-        for(int i = zeroes; i < zeroes+ones; i++){
+        for(int i = numberOfZeroes; i < (numberOfZeroes+numberOfTOnes); i++){
             arr[i] = 1;
         }
-        for(int i = zeroes+ones; i < l; i++){
+        for(int i = (numberOfZeroes+numberOfTOnes); i < l; i++){
             arr[i] = 2;
         }
         return arr;
