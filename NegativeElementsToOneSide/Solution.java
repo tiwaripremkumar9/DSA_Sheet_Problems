307eda7757971e4c580402d114e8ab3857187370
Prem Tiwari <prem.tiwari@finastra.com>
2022-12-21 21:32:50 +0530

    moving negative elements to RHS [this kind of algorithm is also called as partition algo]

--- NegativeElementsToOneSide/Solution.java
+++ NegativeElementsToOneSide/Solution.java
@@ -4,12 +4,27 @@ import java.util.Arrays;
 
 public class Solution {
 
+    // public static int[] negativesAtOneEnd(int [] arr){
+    //     int pivot = -1; // pivot pointer
+    //     for(int j = 0; j < arr.length; j++){
+    //         if(arr[j] < 0){
+    //             int temp = 0;
+    //             pivot++;
+    //             temp = arr[pivot];
+    //             arr[pivot] = arr[j];
+    //             arr[j] = temp;
+    //         }
+    //     }
+    //     return arr;
+    // }
+
     public static int[] negativesAtOneEnd(int [] arr){
-        int pivot = -1; // pivot pointer
-        for(int j = 0; j < arr.length; j++){
+        int l = arr.length;
+        int pivot = l; // pivot pointer
+        for(int j = l-1; j >= 0; j--){
             if(arr[j] < 0){
                 int temp = 0;
-                pivot++;
+                pivot--;
                 temp = arr[pivot];
                 arr[pivot] = arr[j];
                 arr[j] = temp;
