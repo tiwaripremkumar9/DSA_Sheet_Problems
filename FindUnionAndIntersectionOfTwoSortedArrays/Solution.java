76e392ec7be4e1d29d0a09a1779e0ad877084414
Prem Tiwari <prem.tiwari@finastra.com>
2022-12-26 13:58:50 +0530

    Union using Merge

--- FindUnionAndIntersectionOfTwoSortedArrays/Solution.java
+++ FindUnionAndIntersectionOfTwoSortedArrays/Solution.java
@@ -1,10 +1,49 @@
 package FindUnionAndIntersectionOfTwoSortedArrays;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Solution {
+
+    public static int[] unionUsingMerge(int [] arr1, int [] arr2){
+            int i = 0, j = 0;
+            List<Integer> list = new ArrayList<>();
+            while(i < arr1.length && j < arr1.length) {
+                if(i == arr1.length){
+                    list.add(arr2[j]);
+                    j++;
+                    continue;
+                }
+                if(j == arr2.length){
+                    list.add(arr1[i]);
+                    i++;
+                    continue;
+                }
+
+                if(arr1[i] < arr2[j]){
+                    list.add(arr1[i]);
+                    i++;
+                }
+                if(arr1[i] > arr2[j]){
+                    list.add(arr2[j]);
+                    j++;
+                }
+                if(arr1[i] == arr2[j]){
+                    list.add(arr2[j]);
+                    i++;
+                    j++;
+                }
+            }
+            int k = 0;
+            int [] arr = new int[list.size()];
+            for (Integer each : list) {
+                arr[k++] = each;
+            }
+            return arr;
+    }
 
     public static int []  unionUsingSets(int [] arr1, int [] arr2){
         Set<Integer> set = new HashSet<Integer>();
@@ -27,6 +66,6 @@ public class Solution {
     public static void main(String[] args) {
         int [] arr1 = {1,4,5,7,8,9};
         int [] arr2 = {2,3,5,6,7,9};
-        System.out.println(Arrays.toString(unionUsingSets(arr1, arr2)));
+        System.out.println(Arrays.toString(unionUsingMerge(arr1, arr2)));
     }
 }
